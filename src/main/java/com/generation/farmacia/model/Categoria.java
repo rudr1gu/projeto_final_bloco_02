4455b69286bc60294c926db29e8e7afc97365639
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-16 10:35:11 -0300

    refactor: update Categoria and Produto models to change relationship from one-to-many to many-to-one

--- src/main/java/com/generation/farmacia/model/Categoria.java
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -1,15 +1,12 @@
 package com.generation.farmacia.model;
 
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -34,7 +31,7 @@ public class Categoria {
     @Size(min = 5, max = 100)
     private String descricao;
     
-    @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    @ManyToOne
     @JsonIgnoreProperties("categoria")
-    private List<Produto> produto;
+    private Produto produto;
 }
--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,12 +1,16 @@
 package com.generation.farmacia.model;
 
+import java.util.List;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -33,7 +37,7 @@ public class Produto {
     @NotNull
     private int quantidade;
     
-    @ManyToOne
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "produto", cascade = CascadeType.ALL)
     @JsonIgnoreProperties("produto")
-    private Categoria categoria;
+    private List<Categoria> categorias;
 }
