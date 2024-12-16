f917709f12af6a13cd86897414de87e6732fea97
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-16 09:37:34 -0300

    :sparkles: config application properties

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,36 @@
+package com.generation.farmacia.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    @NotNull
+    @Size(min = 5, max = 100)
+    private String descricao;
+    
+    // @OneToMany(mappedBy = "categoria", cascade = CascadeType.ALL)
+    // @JsonIgnoreProperties("categoria")
+    // private List<Produto> produto;
+}
