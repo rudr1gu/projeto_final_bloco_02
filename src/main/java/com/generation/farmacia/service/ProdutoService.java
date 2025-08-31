80c2880159cddb262918b665ea55d502dfc248bc
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-08-31 10:46:12 -0300

    :sparkles: feat: add ProdutoService for managing Produto entities

---
+++ src/main/java/com/generation/farmacia/service/ProdutoService.java
@@ -0,0 +1,32 @@
+package com.generation.farmacia.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.generation.farmacia.model.Produto;
+import com.generation.farmacia.repository.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public List<Produto> findAll() {
+        return produtoRepository.findAll();
+    }
+
+    public Produto findById(Long id) {
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    public Produto save(Produto produto) {
+        return produtoRepository.save(produto);
+    }
+
+    public void deleteById(Long id) {
+        produtoRepository.deleteById(id);
+    }
+}
